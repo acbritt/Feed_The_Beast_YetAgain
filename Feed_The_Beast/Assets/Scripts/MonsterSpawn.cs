807f184d7d23982919692dc46fd2dc38a9084cbb
ValzeGamez <vpfeffer@mail.csuchico.edu>
2022-10-24 19:57:58 -0700

    Debugged progress bar

--- Feed_The_Beast/Assets/Scripts/MonsterSpawn.cs
+++ Feed_The_Beast/Assets/Scripts/MonsterSpawn.cs
@@ -7,61 +7,72 @@ public class MonsterSpawn : MonoBehaviour
 
     public Board score;
 
-    public bool eggSpawned;
+    public bool stage1;
     public bool stage2;
     public bool stage3;
 
     //references to the monster game objects
-    public GameObject Egg_model;
-    public GameObject MonsterStage2;
-    public GameObject MonsterStage3;
+    public GameObject Stage1;
+    public GameObject Stage2;
+    public GameObject Stage3;
 
+    public bool P_Bar_Stage2;
+    public bool P_Bar_Stage3;
+
+    public int MStage2TargetScore;
+    public int MStage3TargetScore;
 
     // Start is called before the first frame update
     void Start()
     {
         score = GameObject.Find("Board").GetComponent<Board>();
 
-        eggSpawned = false;
-
+        //toggles the spawn to prevent duplicates from spawning
+        stage1 = true;
         stage2 = false;
-
         stage3 = false;
-      
+
+        // bool to tell the evoluton progress bar when to readjust it's maximum amount
+        P_Bar_Stage2 = false;
+        P_Bar_Stage3 = false;
+
+        Stage1 = Instantiate(Stage1, new Vector3(1.5f, 3.4f, 1.15f), Quaternion.identity);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (score.MatchScore >= 50  && eggSpawned == false)
+        if (score.MatchScore >= MStage2TargetScore && stage1 == true)
         {
-            Destroy(Egg_model);
+            Destroy(Stage1);
+
+            Stage2 = Instantiate(Stage2, new Vector3(1.5f, 3.4f, 1.15f), Quaternion.identity);
+
+            stage1 = false;
+
+            P_Bar_Stage2 = true;
+
+           
 
         }
 
-       
 
 
-
-       /* if (score.MatchScore >= 8 && stage2 == false)
+        if (score.MatchScore >= MStage3TargetScore && stage2 == false)
         {
-            var Monster2 = Instantiate(MonsterStage2, new Vector3(1.5f, 3.4f, 1.15f), Quaternion.identity);
-            Debug.Log("Event triggered");
-            Destroy(Egg_model);
-            Instantiate(Monster2);
+            Destroy(Stage2);
+
+            Stage2 = Instantiate(Stage3, new Vector3(1.5f, 3.4f, 1.15f), Quaternion.identity);
+
             stage2 = true;
 
-            if (score.MatchScore >= 20 && stage3 == false)
-            {
-                var Monster3 = Instantiate(MonsterStage3, new Vector3(1.5f, 3.4f, 1.15f), Quaternion.identity);
-                Destroy(Monster2);
-                Instantiate(Monster3);
-                stage3 = true;
-            }
+            P_Bar_Stage2 = false;
 
+            P_Bar_Stage3 = true;
 
         }
-        */
+
 
 
     }
--- Feed_The_Beast/Assets/Scripts/ProgressBar.cs
+++ Feed_The_Beast/Assets/Scripts/ProgressBar.cs
@@ -8,6 +8,9 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
 {
 
+    public bool resetToZero;
+
+
     public int maximum;
 
     public int current;
@@ -16,14 +19,18 @@ public class ProgressBar : MonoBehaviour
 
     public Board Board;
 
-    
+    public MonsterSpawn monster;
 
     // Start is called before the first frame update
     void Start()
     {
+        resetToZero = false;
+
         current = 0;
 
         gameObject.GetComponent<Board>();
+
+        maximum = monster.MStage2TargetScore;
     }
 
     // Update is called once per frame
@@ -31,7 +38,23 @@ public class ProgressBar : MonoBehaviour
     {
         GetCurrentFill();
 
+        //sets the bar to always show the current score
         current = Board.MatchScore;
+
+ //     changes the max value of the progress bar to match the
+ //     target score for the next stage:       
+        if(monster.P_Bar_Stage2 == true  && resetToZero == false)
+        {
+            //resets bar so the points don't carry over
+            Board.MatchScore = 0;
+
+            maximum = monster.MStage3TargetScore;
+
+ //         breaks the update loop so it doesn't 
+ //         infinitely reset bar to 0
+            resetToZero = true;
+           
+        }
        
     }
 
