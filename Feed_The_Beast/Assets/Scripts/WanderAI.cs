6ab470107a51f7e37559d91ad71980030d67e656
MarcusG0523 <101525932+MarcusG0523@users.noreply.github.com>
2022-10-27 02:23:52 -0700

    redo everything

---
+++ Feed_The_Beast/Assets/Scripts/WanderAI.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public float rotSPeed = 100f;
+    private bool isWandring = false;
+    private bool isRotatingLeft = false;
+    private bool isRotatingRight = false;
+    private bool isWalking = false;
+// Start is called before the first frame update
+void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isWandring == false)
+        {
+            StartCoroutine(Wander());
+        }
+        if(isRotatingRight == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * moveSpeed);
+        }
+        if (isRotatingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotSPeed);
+        }
+        if(isWalking == true)
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+    }
+    IEnumerator Wander()
+    {
+        int rotTime = Random.Range(1, 3);
+        int rotateWait = Random.Range(3, 4);
+        int rotateLorR = Random.Range(1, 2);
+        int walkWait = Random.Range(1, 4);
+        int walkTime = Random.Range(1, 5);
+
+        isWandring = true;
+
+        yield return new WaitForSeconds(walkWait);
+        isWalking = true;
+        yield return new WaitForSeconds(walkTime);
+        isWalking = false;
+        yield return new WaitForSeconds(rotateWait);
+        if(rotateLorR == 1)
+        {
+            isRotatingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingRight = false;
+
+        }
+        if (rotateLorR == 2)
+        {
+            isRotatingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            isRotatingLeft = false;
+
+        }
+        isWandring = false;
+    }
+}
